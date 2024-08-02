ee554775241a43b0ccc795520799419ec44627ac
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-02 11:56:45 +1000

    Add LendingController to maintain all lending related APIs.

--- src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
+++ src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
@@ -1,2 +1,10 @@
-package com.p2p_lending.p2p_backend.controllers;public class LendingController {
+package com.p2p_lending.p2p_backend.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class LendingController {
+
 }
