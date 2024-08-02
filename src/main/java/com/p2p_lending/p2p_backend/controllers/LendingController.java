73499b52537fead7f609734b12b68d229d7e7e0d
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-02 19:07:21 +1000

    Add actuator and bypass auth for localhost connections.

--- src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
+++ src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
@@ -1,10 +1,10 @@
 package com.p2p_lending.p2p_backend.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class LendingController {
-
+    
 }
