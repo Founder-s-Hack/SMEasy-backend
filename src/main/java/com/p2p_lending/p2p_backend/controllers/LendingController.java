12be164cfd412df88f6a3b506384859d576807c8
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-03 13:24:36 +1000

    Remove dummy code and integrate swagger.

--- src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
+++ src/main/java/com/p2p_lending/p2p_backend/controllers/LendingController.java
@@ -1,31 +1,10 @@
 package com.p2p_lending.p2p_backend.controllers;
 
-import com.p2p_lending.p2p_backend.models.User;
-import com.p2p_lending.p2p_backend.services.LendingService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class LendingController {
-
-    private final LendingService lendingService;
-
-    public LendingController(LendingService lendingService) {
-        this.lendingService = lendingService;
-    }
-
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "Test Successful";
-    }
-
-    @PostMapping("/post-test")
-    public String postEndpoint(@RequestBody User user) {
-        return this.lendingService.processUser(user);
-    }
 
 }
