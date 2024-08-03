bea88987b4821e5f260c29256bb14267361cf265
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-03 15:36:04 +1000

    Add openfeign and enable feign clients.

--- src/main/java/com/p2p_lending/p2p_backend/P2PBackendApplication.java
+++ src/main/java/com/p2p_lending/p2p_backend/P2PBackendApplication.java
@@ -2,8 +2,10 @@ package com.p2p_lending.p2p_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class P2PBackendApplication {
 
     public static void main(String[] args) {
