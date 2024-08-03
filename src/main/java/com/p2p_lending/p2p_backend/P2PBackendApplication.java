61da35f689e88db3c522a57d886054545b242aa3
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-04 01:12:32 +1000

    Add mongo connection to spring boot

--- src/main/java/com/p2p_lending/p2p_backend/P2PBackendApplication.java
+++ src/main/java/com/p2p_lending/p2p_backend/P2PBackendApplication.java
@@ -3,9 +3,11 @@ package com.p2p_lending.p2p_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableMongoRepositories
 public class P2PBackendApplication {
 
     public static void main(String[] args) {
